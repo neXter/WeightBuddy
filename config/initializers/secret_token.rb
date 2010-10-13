c7a0508c169f832fe938b2e9e585114806355099
Pieter Walsweer <pieter@walsweer.de>
2010-10-13 21:40:39 +0200

    Initial commit of unmodified new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeightBuddy::Application.config.secret_token = 'bd80b65bf4af306727548e229342fcd585a012b6b5d60ee6061bf51a30cfd2d7d4a468322593b1ad0768e8b21126d3bb7093ca96f5af0c7063c7327adb23d03f'
