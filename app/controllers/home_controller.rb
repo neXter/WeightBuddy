27eba0f6fa40edcd3395c237dbadcfc954ef0b5d
Pieter Walsweer <pieter@walsweer.de>
2010-10-13 23:01:44 +0200

    first mobile responsive version

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   def index
     @users = User.all
 
+    respond_to do |format|
+       format.html
+       format.mobile { render :layout => false }
+     end
   end
 
 end
